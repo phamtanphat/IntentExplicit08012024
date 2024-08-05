d660d65f828808511e9c4229e6860625a1511235
phatdroid94 <phatdroid94@gmail.com>
2024-08-05 18:27:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/intentexplicit08012024/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.intentexplicit08012024
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private var btnSendData : Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnSendData = findViewById(R.id.button_send_data_by_intent)
+        btnSendData?.setOnClickListener {
+
+        }
+    }
+}
